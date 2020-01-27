a46bcf3b334c0eac12e1b80f95d8ffb2540f29e4
Jaysonliu <leobest9688@gmail.com>
2020-01-27 22:04:52 +0800

    fix the code style

--- server.js
+++ server.js
@@ -28,13 +28,6 @@ app.set("view engine", "handlebars");
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-// var syncOptions = { force: false };
-
-// If running a test, set syncOptions.force to true
-// clearing the `testdb`
-// if (process.env.NODE_ENV === "test") {
-//   syncOptions.force = true;
-// }
 
 const syncOptions = { force: true };
 
